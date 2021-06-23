4badd044e7cc1ff5616e7e33e986ece285183944
Francesco <francescococlite@gmail.com>
2021-06-23 15:03:01 +0200

    add funzione click

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 var app = new Vue({
     el: '#app',
     data: {
-      
+        nuovoImpegno:'',
+        impegni:[
+            'Fare la spesa',
+            'Pulire la macchina',
+            'Allenarsi',
+            'Compleanno amico/a'
+          
+        ]
+    },
+    methods: {
+        addImpegno(){
+            this.impegni.push(this.nuovoImpegno)
+        }
     }
 })
